940d97b6ab51c13e3f9b63c8bb45a039c1364969
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-08-25 20:13:41 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,18 +1,23 @@
 #/bin/bash
 clear
 echo "script by Thavanish"
-PS3='select you prefered method:[GUI install | CLI install]'
-options=("GUI install" "CLI install" )
+PS3='select you prefered method:[GUI install | CLI install | Exit]'
+options=("GUI install" "CLI install" "Exit" )
 select opt in "${options[@]}"
 do
     case $opt in
         "GUI install")
         chmod +x scripts/ui/installergui.sh
         bash scripts/ui/installergui.sh
+        exit
         ;;
         "CLI install")
         chmod +x scripts/ui/installercli.sh
         bash scripts/ui/installercli.sh
+        exit
+        ;;
+        "Exit")
+        exit
         ;;
         *) echo "invalid option $REPLY";;
     esac
