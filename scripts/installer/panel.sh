295d847e10ef4ccdc7eddfbabbf92c82414a3590
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-08-25 20:50:50 +0530

    Update panel.sh

--- scripts/installer/panel.sh
+++ scripts/installer/panel.sh
@@ -1,3 +1,10 @@
+echo "the files will be deleted at the destination of install......"
+sleep 10
+sudo rm -rf /etc/skyport/*
+sudo rm -rf /etc/skyport/*
+sudo rm -rf /etc/skyport/*
+sudo rm -rf /etc/skyport/*
+
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
