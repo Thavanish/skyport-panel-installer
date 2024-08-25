8326367981f6e46e10246db850051fe3a312ae51
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-08-25 18:39:10 +0530

    Create daemon.sh

---
+++ daemon.sh
@@ -0,0 +1,13 @@
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt update
+sudo apt install -y nodejs git
+cd /etc
+git clone --branch v0.2.2 https://github.com/skyportlabs/skyportd
+cd skyportd
+npm install
+clear
