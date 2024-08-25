5d43f6acc1cf12ed660fb9fda74511472c1bc4b1
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-08-25 18:41:48 +0530

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -1,18 +1,18 @@
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt update
 sudo apt install -y nodejs git
-cd /etc
-git clone --branch v0.2.1 https://github.com/skyportlabs/panel
-mv panel skyport
-cd skyport
-npm install
+sudo cd /etc
+sudo git clone --branch v0.2.1 https://github.com/skyportlabs/panel
+sudo mv panel skyport
+sudo cd skyport
+sudo npm install
 clear
-npm run seed
-npm run createUser
-clear
+sudo npm run seed
+sudo npm run createUser
+
 echo "use 'node .' to start the panel!"
 echo "sleeping for 5 sec"
 sleep 5
