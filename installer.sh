61f04fd51e6f6ed33004edced522eb8d8a1c32a1
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-08-25 18:37:04 +0530

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+clear
+echo "starting install......"
+#!/bin/bash
+echo "1)install panel 2)install daemon 3)install both 4)remove panel 5)remove daemon 6)quit"
+PS3='What do you want to do ?: '
+options=("instal1 panel" "install daemon" "install both" "remove panel" "remove daemon" "quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "instal1 panel")
+            clear
+            echo "install panel....."
+            chmod +x panel
+            ./panel.sh
+            
+            ;;
+        "install daemon")
+            clear          
+            echo "install daemon........"
+            chmod +x daemon.sh
+            ./daemon.sh
+            
+            ;;
+        "install both")
+            clear
+            echo "installing panel and daemon......"
+            clear
+            echo "install panel....."
+            chmod +x panel.sh
+            ./panel.sh
+            clear          
+            echo "install daemon........"
+            chmod +x daemon.sh
+           ./daemon.sh
+           
+           ;;
+        "remove panel")
+            clear
+            echo "removing panel.."
+            sudo rm -rf /etc/skyport/*
+            
+            ;;
+        "remove daemon")
+            clear
+            echo "removing daemon.."
+            sudo rm -rf /etc/skyportd/*
+            
+            ;;
+        "quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
